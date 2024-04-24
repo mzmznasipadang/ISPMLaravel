3398a98fa69f3d636b8bed2c98bc36d5525edccf
erlnrinzl <henrichfergian@gmail.com>
2024-04-24 13:00:23 +0700

    refractor view code and implement bootstrap library

---
+++ resources/views/partials/ispm-section.blade.php
@@ -0,0 +1,29 @@
+<div class="row" id="section-3">
+  <div class="col-md-6" id="hero">
+      <img src="/image/image-8.png" alt="">
+  </div>
+  <div class="col-md-6 mb-4" id="content">
+      <h1 class="mt-3">Information Systems Project <br> Member </h1>
+      <p>
+          Information Systems Project Members are members of the
+          <br> 
+          Information Systems Laboratory concerned with developing their
+          <br>
+          skills in real-world projects. In order to accomplish this, Project
+          <br>
+          Members are given training on web development and research
+          <br>
+          before then implementing it on real-world projects by the IS Lab 
+          <br>
+          and academic research to be published.
+          <br>
+          <br>
+          Furthermore, Project Members are also given the chance to show
+          <br>
+          their technical skills in web development competitions, while
+          <br>
+          receiving guidance from senior IS Lab members.
+      </p>
+      <button class="btn">Register Now</button>
+  </div>
+</div>
