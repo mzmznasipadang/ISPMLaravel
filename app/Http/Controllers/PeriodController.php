cee02b439430084f35df964716e0d409b6148998
Victor Chandra <41635105+mzmznasipadang@users.noreply.github.com>
2024-05-07 21:40:38 +0700

    gajadi guis batal

--- app/Http/Controllers/PeriodController.php
+++ app/Http/Controllers/PeriodController.php
@@ -17,7 +17,7 @@ class PeriodController extends Controller
     public function index()
     {
         return view('landing-page', [
-            // "periods" => Period::all(),
+            "periods" => Period::all(),
         ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('landing-page', [
-        // "test" => content::all(),
+        "test" => content::all(),
     ]);
 });
 
