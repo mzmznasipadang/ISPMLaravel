540e8c4c66c59e33c2ad09e9a7316b7c54dc7678
Kenneth <kennethjriady@gmail.com>
2024-05-10 20:32:22 +0700

    microsoft authentication

--- resources/views/partials/footer.blade.php
+++
@@ -1,8 +0,0 @@
-<footer class="container-fluid text-end d-flex align-items-center justify-content-end" style="height: 150px; background-color:rgb(31, 32, 32)">
-  <div class="row">
-    <div class="col me-4">
-      <h3 class="text-white mt-3">SCHOOL OF INFORMATION SYSTEM</h3>
-      <p class="text-white ">Copyright © BINUS Higher Education. All rights reserved</p>
-    </div>
-  </div>
-</footer>
